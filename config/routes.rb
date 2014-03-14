4af7f53e75a601c3c8fd6910cb02822f6fc87415
Augusto Pimenta <augustoppimenta@gmail.com>
2014-03-14 20:29:57 -0300

    Criação de layout

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Pomo::Application.routes.draw do
 
   root 'home#index'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
